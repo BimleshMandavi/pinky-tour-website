114483fc56393e21a3aab948c6695a6406116553
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2024-08-14 19:31:18 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-white">
-    ankit
-    </div>
+   <main>
+    <section>
+      
+    </section>
+   </main>
   );
 }
